dd98d5af13d95b034751d6a2a48ed6ea3165a4dd
Danila <tess4184@gmail.com>
2025-02-21 19:23:00 +0300

    "Fix Babel dynamic import issue"

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,3 +1,14 @@
 export default {
-  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+  presets: [
+    ["@babel/preset-env", { loose: true }],
+    "@babel/preset-react",
+    "@babel/preset-typescript"
+  ],
+  plugins: [
+    "@babel/plugin-syntax-dynamic-import", 
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }]
+  ]
 };
