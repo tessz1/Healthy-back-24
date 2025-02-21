5ec34fa1d19f600b4227f28ab69c43c9ccf558a7
Danila <tess4184@gmail.com>
2025-02-21 22:06:12 +0300

    "memory"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import babel from 'vite-plugin-babel';
 
 export default defineConfig({
+  build: {
+    minify: false, // Выключает минификацию (можно включить обратно после теста)
+    sourcemap: false, // Отключает source maps
+    chunkSizeWarningLimit: 2000,
+  },
   server: {
     // Разрешенные хосты для разработки
     allowedHosts: ['0f31-2607-740-22-5-9845-4aae-babc-38e3.ngrok-free.app'],
