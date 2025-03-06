b15c34a079e690d45d967873eeebf2695df1cb00
Danila <tess4184@gmail.com>
2025-03-06 14:06:00 +0300

    "change navigation"

--- client/src/pages/admin/NavigationAdmin.tsx
+++ client/src/pages/admin/NavigationAdmin.tsx
@@ -3,26 +3,22 @@ import { Link } from "react-router-dom";
 
 function NavigationAdmin() {
   return (
-    <div className="bg-black min-h-screen p-4 font-roboto">
-      <div className="flex flex-col items-center mt-24 sm:mt-20">
-        <ul className="w-full max-w-md space-y-4 ">
-          <li className="rounded-xl p-2 bg-orange-400 text-white text-sm sm:text-base text-center hover:bg-orange-500 transition duration-300 sm:w-1/4">
-            <Link to="/uploadCourse" className="block w-full h-full">
+    <div className="bg-gray-900 min-h-screen p-4 font-roboto flex justify-center items-center">
+      <div className="w-full max-w-4xl">
+        <ul className="space-y-6">
+          <li className="rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105">
+            <Link
+              to="/uploadCourse"
+              className="block bg-gradient-to-r from-orange-400 to-orange-600 text-white text-lg sm:text-xl font-semibold py-4 px-6 text-center hover:bg-gradient-to-r hover:from-orange-500 hover:to-orange-700"
+            >
               Добавление курсов в магазин
             </Link>
           </li>
-          <li className="rounded-xl p-2 bg-orange-400 text-white text-sm sm:text-base text-center hover:bg-orange-500 transition duration-300 sm:w-1/4">
-            <Link to="/add-content" className="block w-full h-full">
-              Добавление контента в курсы
-            </Link>
-          </li>
-          <li className="rounded-xl p-2 bg-orange-400 text-white text-sm sm:text-base text-center hover:bg-orange-500 transition duration-300 sm:w-1/4">
-            <Link to="/delete-course" className="block w-full h-full">
-              Удаление курса
-            </Link>
-          </li>
-          <li className="rounded-xl p-2 bg-orange-400 text-white text-sm sm:text-base text-center hover:bg-orange-500 transition duration-300 sm:w-1/4">
-            <Link to="/createPromoCode" className="block w-full h-full">
+          <li className="rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105">
+            <Link
+              to="/createPromoCode"
+              className="block bg-gradient-to-r from-purple-400 to-purple-600 text-white text-lg sm:text-xl font-semibold py-4 px-6 text-center hover:bg-gradient-to-r hover:from-purple-500 hover:to-purple-700"
+            >
               Промокоды
             </Link>
           </li>
